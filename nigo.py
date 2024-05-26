57e41609a1d476ae2d85f7d3039d6e323cb13151
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2024-05-26 13:13:50 +0300

    Add files via upload

--- nigo.py
+++ nigo.py
@@ -19,19 +19,21 @@ class main_Cod(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         layaut = BoxLayout(orientation = "vertical")
-        a = VideoPlayer(source='git.mp4', state='play',
-        options={'fit_mode': 'contain'})
-        layaut.add_widget(a)
-        button = Temate_button(self,direction="down",goal="nw",text="dd")
+        label = Label(text="main window")
+        layaut.add_widget(label)
+        button = Temate_button(self,direction="down",goal="nw",text="Borderlands")
         layaut.add_widget(button)
         self.add_widget(layaut)
 
-class new_window(Screen):
+class new_fil_Borderlands(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         layaut = BoxLayout(orientation = "vertical")
-        button = Temate_button(self,direction="up",goal="main",text="dd")
-        label = Label(text="nigger")
+        button = Temate_button(self,direction="up",goal="main",text="main")
+        a = VideoPlayer(source='borde.mp4', state='pause',
+        options={'fit_mode': 'contain'})
+        layaut.add_widget(a)
+        label = Label(text_size =(600,None),text="Про фільм за мотивами однойменної серії відеоігор від Gearbox Software ми вже писали у Топі майбутніх екранізацій забавок. Елай Рот займався режисурою, потім було двотижневе перефільмування, на якому його замінив Тім Міллер. Влітку стало відомо, що Крейг Мазін не є автором чи співавтором сценарію, як зазначалося раніше. Зрештою, якийсь час проєкт знаходився у поствиробничому пеклі, що напевно позначиться на якості продукту, але, здається, студія Lionsgate нарешті готова випустити його на екрани.За сюжетом сумнозвісна злочинниця із загадковим минулим Ліліт (Кейт Бланшетт) неохоче повертається на свою рідну планету Пандору і укладає несподівану угоду. Вона береться знайти зниклу доньку Атласа, об’єднавши зусилля з іншими персонажами гри. Усі вони вирушають у сповнену небезпек пригоду.")
         layaut.add_widget(label)
         layaut.add_widget(button)
         self.add_widget(layaut)
@@ -39,7 +41,7 @@ class myapp(App):
     def build(self):
         manager = ScreenManager()
         manager.add_widget(main_Cod(name="main"))
-        manager.add_widget(new_window(name="nw"))
+        manager.add_widget(new_fil_Borderlands(name="nw"))
         return manager
     
 app = myapp()
